8c6a970e67d842d8fea8a5ea0ce7625f872f8a29
rogerscj <rogerscj@rose-hulman.edu>
2019-02-04 12:59:10 -0500

    done

--- src/m3_robot_as_mqtt_receiver.py
+++ src/m3_robot_as_mqtt_receiver.py
@@ -5,3 +5,5 @@ Using a Brickman (robot) as the receiver of messages.
 # Same as m2_fake_robot_as_mqtt_sender,
 # but have the robot really do the action.
 # Implement just FORWARD at speeds X and Y is enough.
+
+# on partners computor
